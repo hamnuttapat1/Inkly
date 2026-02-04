23d476058232e65ada9bf8a72c3688cf97e4ec68
hamnuttapat1 <67011213@kmitl.ac.th>
2026-02-04 13:34:14 +0700

    update/note_form

--- frontend/src/components/note_forum/note_forum_top_bar.jsx
+++ frontend/src/components/note_forum/note_forum_top_bar.jsx
@@ -7,7 +7,7 @@ const Note_forum_top_bar = () => {
         <div className='w-full h-51.75 bg-white shadow-md '>
             <div className='flex flex-row mt-2 gap-3 items-center'>
                 <BiMessageSquare size={40} className='opacity-70 ml-7 mt-2' />
-                <p className='font-["Julius Sans One"] text-[40px] text-[#3E4A34] font-thin'>DISCUSSION FORUM</p>
+                <p className='font-["Julius Sans One"] text-[40px] text-[#3E4A34] font-thin'>NOTE FORUM</p>
             </div>
             <div className='ml-7 mr-7 mt-2 flex flex-row gap-3 w- items-center border-2 rounded-3xl p-1 pl-5 '>
                 <button className='cursor-pointer'>
