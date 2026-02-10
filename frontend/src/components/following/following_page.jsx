085b9a1ccc7420afa1d1e810ccd2d30b057d7af5
hamnuttapat1 <67011213@kmitl.ac.th>
2026-02-10 20:05:25 +0700

    update/follow

--- frontend/src/components/following/following_page.jsx
+++ frontend/src/components/following/following_page.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Following_page = () => {
-  return (
-    <div className='w-full h-full bg-[#EEF2E1] overflow-auto'>
-      <div>
-        <p>Following</p>
-      </div>
-    </div>
-  )
+    return (
+        <div className='w-full h-full bg-[#EEF2E1] overflow-auto'>
+            <div className='flex flex-row my-[20px] ml-[50px] font-["Inter"] text-[24px] text-[#124C09]/60 font-regular'>
+                <p>Blogs You follow(</p>
+                <p>10</p>
+                <p>)</p>
+            </div>
+        </div>
+    )
 }
 
 export default Following_page
