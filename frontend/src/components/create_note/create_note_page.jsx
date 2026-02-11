a4f886021ff6a5417960c5fcb46d5602e12d5368
KLAharris <harriskla222@gmail.com>
2026-02-12 02:23:23 +0700

    update/create_note

--- frontend/src/components/create_note/create_note_page.jsx
+++ frontend/src/components/create_note/create_note_page.jsx
@@ -4,7 +4,7 @@ const Create_note_page = () => {
   return (
     <div className='w-full h-full bg-[#EEF2E1] overflow-auto'>
       <div className=''>
-        <p>Create note</p>
+        <p>Kla</p>
       </div>
     </div>
   )
